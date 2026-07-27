49eefa26ca8cd7c65d0014e6ed9817691429ab19
Aaryanverma <aryan212verma@gmail.com>
2026-07-27 18:36:27 +0530

    Update workflows and project configuration for improved package management and versioning

--- graybox/__init__.py
+++ graybox/__init__.py
@@ -1,1 +1,7 @@
-__version__ = "0.1.0"
+# graybox/__init__.py
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("graybox")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
